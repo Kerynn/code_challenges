ef52395848b5e67eb99182da5e5bef34a04ee4b5
Kerynn Davis <kerynn.davis@gmail.com>
2023-05-31 15:37:35 -0700

    solve diagonal_difference

--- lib/diagonal_difference.rb
+++ lib/diagonal_difference.rb
@@ -1,6 +1,20 @@
 require 'pry'
 
-def diagonalDifference(arr)
-
-
+def diagonalDifference(matrix)
+  n = matrix.size
+  l_sum = 0
+  r_sum = 0
+  
+  (0...n).each do |i|
+    l_sum += matrix[i][i]
+    r_sum += matrix[i][n - 1 - i]
+  end
+  
+  (l_sum - r_sum).abs
 end
+
+matrix = [[11, 2, 4],
+          [4, 5, 6],
+          [10, 8, -12]]
+
+p diagonalDifference(matrix)
